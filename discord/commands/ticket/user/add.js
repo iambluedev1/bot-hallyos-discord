363fef6b1daa583f98cafb9a9ccc38e95150809d
Guillaume <iambluedev@gmx.fr>
2021-04-12 17:16:20 +0200

    added possibility to add or remove someone from tickets

--- discord/commands/ticket/user/add.js
+++ discord/commands/ticket/user/add.js
@@ -1,9 +1,99 @@
 module.exports = {
   command: 'add',
   description: 'Ajouter un utilisateur au ticket',
-  usage: "ticket user add (:word|<nom d'utilisateur>)",
+  usage: "ticket user add (:tag|<nom d'utilisateur>)",
   roles: hallyos.config.discord.support.roles,
-  execute: async (args, channel, member, message) => {
-    message.guild.members.fetch().then((e) => console.log(e));
+  execute: async (args, channel, cmder, message) => {
+    let ticket = await db('hallyos_tickets').where({
+      channel_id: channel.id,
+    });
+
+    if (!ticket) {
+      const m = await hallyos.discord.client.sendError(
+        'Erreur',
+        'Vous ne pouvez éxecuter cette commande que depuis un ticket !',
+        channel
+      );
+      setTimeout(() => {
+        m.delete();
+        message.delete();
+      }, 5000);
+      return;
+    }
+
+    ticket = ticket[0];
+
+    const guild = hallyos.discord.client.guilds.resolve(
+      hallyos.config.discord.guildId
+    );
+    let find = false;
+    guild.members.cache.array().forEach((member) => {
+      if (
+        args[0] == member.user.username ||
+        args[0] == member.user.username + '#' + member.user.discriminator
+      ) {
+        find = true;
+
+        db('hallyos_tickets_externals')
+          .insert({
+            ticket_id: ticket.id,
+            discord_id: member.id,
+            username: member.user.username,
+            discord_added_by: cmder.user.username,
+          })
+          .then(() => {
+            channel
+              .updateOverwrite(member, {
+                ADD_REACTIONS: true,
+                SEND_MESSAGES: true,
+                READ_MESSAGE_HISTORY: true,
+                ATTACH_FILES: true,
+                EMBED_LINKS: true,
+                VIEW_CHANNEL: true,
+              })
+              .then(async () => {
+                hallyos.helpers.record.discordAction(
+                  cmder,
+                  'ADD_USER_TO_TICKET',
+                  args[0] + ';' + ticket.id
+                );
+                hallyos.discord.client.sendInfo(
+                  `Nouveau participant`,
+                  `${cmder} vient d'ajouter ${member} au ticket`,
+                  channel
+                );
+                hallyos.log.info(
+                  `Successfully added ${member.user.username} to ticket ` +
+                    ticket.id
+                );
+              });
+          })
+          .catch(async (e) => {
+            hallyos.log.warn(e);
+            const m = await hallyos.discord.client.sendError(
+              'Erreur',
+              `L'utilisateur ${member.user.username} a déjà été ajouté au ticket !`,
+              channel
+            );
+            setTimeout(() => {
+              m.delete();
+            }, 5000);
+          });
+        return;
+      }
+    });
+
+    message.delete();
+
+    if (!find) {
+      const message = await hallyos.discord.client.sendWarn(
+        'Utilisateur introuvable',
+        'Cette utilisateur est introuvable !',
+        channel
+      );
+      setTimeout(() => {
+        message.delete();
+      }, 5000);
+    }
   },
 };
--- discord/commands/ticket/user/remove.js
+++ discord/commands/ticket/user/remove.js
@@ -1,7 +1,101 @@
 module.exports = {
   command: 'remove',
   description: 'Retirer un utilisateur du ticket',
-  usage: "ticket user remove (:word|<nom d'utilisateur>)",
+  usage: "ticket user remove (:tag|<nom d'utilisateur>)",
   roles: hallyos.config.discord.support.roles,
-  execute: async (args, channel, member, message) => {},
+  execute: async (args, channel, cmder, message) => {
+    let ticket = await db('hallyos_tickets').where({
+      channel_id: channel.id,
+    });
+
+    if (!ticket) {
+      const m = await hallyos.discord.client.sendError(
+        'Erreur',
+        'Vous ne pouvez éxecuter cette commande que depuis un ticket !',
+        channel
+      );
+      setTimeout(() => {
+        m.delete();
+        message.delete();
+      }, 5000);
+      return;
+    }
+
+    ticket = ticket[0];
+    const guild = hallyos.discord.client.guilds.resolve(
+      hallyos.config.discord.guildId
+    );
+    let find = false;
+    guild.members.cache.array().forEach(async (member) => {
+      if (
+        args[0] == member.user.username ||
+        args[0] == member.user.username + '#' + member.user.discriminator
+      ) {
+        find = true;
+
+        const retrieved = await db('hallyos_tickets_externals').where({
+          ticket_id: channel.id,
+          discord_id: member.id,
+        });
+
+        if (retrieved.length > 0) {
+          channel
+            .updateOverwrite(member, {
+              ADD_REACTIONS: false,
+              SEND_MESSAGES: false,
+              READ_MESSAGE_HISTORY: false,
+              ATTACH_FILES: false,
+              EMBED_LINKS: false,
+              VIEW_CHANNEL: false,
+            })
+            .then(async () => {
+              hallyos.helpers.record.discordAction(
+                cmder,
+                'REMOVE_USER_TO_TICKET',
+                args[0] + ';' + ticket.id
+              );
+              hallyos.discord.client.sendInfo(
+                `Mise à jour participants`,
+                `${cmder} vient de retirer ${member} du ticket`,
+                channel
+              );
+              hallyos.log.info(
+                `Successfully removed ${member.user.username} from ticket ` +
+                  ticket.id
+              );
+              await db('hallyos_tickets_externals')
+                .where({
+                  ticket_id: ticket.id,
+                  discord_id: member.id,
+                })
+                .del();
+            });
+        } else {
+          const m = await hallyos.discord.client.sendError(
+            'Erreur',
+            `L'utilisateur ${member.user.username} n'est pas présent ici !`,
+            channel
+          );
+          setTimeout(() => {
+            m.delete();
+          }, 5000);
+        }
+
+        return;
+      }
+    });
+
+    message.delete();
+
+    if (!find) {
+      const message = await hallyos.discord.client.sendWarn(
+        'Utilisateur introuvable',
+        'Cette utilisateur est introuvable !',
+        channel
+      );
+      setTimeout(() => {
+        message.delete();
+      }, 5000);
+    }
+  },
 };
