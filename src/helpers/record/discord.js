8101ba95d685ffcf5c66f65c0a8df86b41ca4b6e
Guillaume <iambluedev@gmx.fr>
2021-04-12 21:45:17 +0200

    fix: discordAction function, the last fix doesn't work :(

--- src/helpers/record/discord.js
+++ src/helpers/record/discord.js
@@ -2,7 +2,8 @@ module.exports.discordAction = (member, action, datas) => {
   db('hallyos_logs')
     .insert({
       discord_author_id: member.id,
-      discord_author_username: member.user.tag || member.username,
+      discord_author_username:
+        'user' in member ? member.user.tag : member.username,
       author_type: 'DISCORD_MEMBER',
       action_type: action,
       additional_datas: datas || null,
