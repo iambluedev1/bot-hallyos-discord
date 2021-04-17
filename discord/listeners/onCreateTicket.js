4e56ad1d30cdf881380b13ab7bb38ac7acae4d97
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 20:37:02 +0000

    fixed ticket permissions

--- discord/listeners/onCreateTicket.js
+++ discord/listeners/onCreateTicket.js
@@ -1,5 +1,5 @@
 module.exports = {
-  listen: 'createTicket',
+  listen: "createTicket",
   run: async (member) => {
     const guild = hallyos.discord.client.guilds.resolve(
       hallyos.config.discord.guildId
@@ -8,57 +8,68 @@ module.exports = {
       hallyos.config.discord.support.categoryId
     );
     const retrieved = (
-      await db('hallyos_tickets').where({
+      await db("hallyos_tickets").where({
         discord_id: member.id,
         state: 1,
       })
     )[0];
     hallyos.helpers.record.discordAction(
       member,
-      'CREATE_TICKET',
+      "CREATE_TICKET",
       JSON.stringify(retrieved)
     );
     const channel = await guild.channels.create(
-      'ticket-' +
+      "ticket-" +
         member.username.toLocaleLowerCase() +
-        '-' +
-        retrieved.id.split('-')[0],
+        "-" +
+        retrieved.id.split("-")[0],
       {
-        permissionOverwrites: [
+        /*permissionOverwrites: [
           {
             id: member.id,
             allow: [
-              'ADD_REACTIONS',
-              'SEND_MESSAGES',
-              'READ_MESSAGE_HISTORY',
-              'ATTACH_FILES',
-              'EMBED_LINKS',
-              'VIEW_CHANNEL',
+              "ADD_REACTIONS",
+              "SEND_MESSAGES",
+              "READ_MESSAGE_HISTORY",
+              "ATTACH_FILES",
+              "EMBED_LINKS",
+              "VIEW_CHANNEL",
             ],
-            deny: ['MENTION_EVERYONE'],
+            deny: ["MENTION_EVERYONE"],
           },
           {
             id: guild.roles.everyone,
             deny: [
-              'VIEW_CHANNEL',
-              'SEND_MESSAGES',
-              'ADD_REACTIONS',
-              'READ_MESSAGE_HISTORY',
-              'ATTACH_FILES',
-              'USE_EXTERNAL_EMOJIS',
+              "VIEW_CHANNEL",
+              "SEND_MESSAGES",
+              "ADD_REACTIONS",
+              "READ_MESSAGE_HISTORY",
+              "ATTACH_FILES",
+              "USE_EXTERNAL_EMOJIS",
             ],
           },
-        ],
+        ],*/
         parent: categorySupport,
       }
     );
 
-    await db('hallyos_tickets')
+    await channel.lockPermissions();
+
+    await channel.updateOverwrite(member, {
+      ADD_REACTIONS: true,
+      SEND_MESSAGES: true,
+      READ_MESSAGE_HISTORY: true,
+      ATTACH_FILES: true,
+      EMBED_LINKS: true,
+      VIEW_CHANNEL: true,
+    });
+
+    await db("hallyos_tickets")
       .update({ channel_id: channel.id })
       .where({ discord_id: member.id, state: 1 });
 
     hallyos.discord.client.sendInfo(
-      'Ticket',
+      "Ticket",
       `Hey ${member} !
 
       Dis nous en quoi nous pouvons t'aider.
@@ -69,11 +80,14 @@ module.exports = {
 
       @here`,
       channel,
-      'référence: ' + retrieved.id
+      "référence: " + retrieved.id
     );
 
     hallyos.log.info(
-      'Ticket ' + retrieved.id + ' created by ' + member.user.tag
+      "Ticket " +
+        retrieved.id +
+        " created by " +
+        ("user" in member ? member.user.tag : member.username)
     );
   },
 };
