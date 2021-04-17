26ee5a343a2af5f6b2109907170328eac642a35c
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 20:57:13 +0000

    authorize self domain request

--- src/middleware/restrict.js
+++ src/middleware/restrict.js
@@ -1,6 +1,6 @@
 const getIp = (req) => {
-  if (req.headers['cf-connecting-ip']) {
-    return req.headers['cf-connecting-ip'];
+  if (req.headers["cf-connecting-ip"]) {
+    return req.headers["cf-connecting-ip"];
   } else {
     return (
       req.ip || (req.connection && req.connection.remoteAddress) || undefined
@@ -10,17 +10,25 @@ const getIp = (req) => {
 
 module.exports = async (req, res, next) => {
   if (
-    (req.get('host') || '') == 'localhost:' + hallyos.config.port ||
+    (req.get("host") || "") == "localhost:" + hallyos.config.port ||
     req.query.bot
   ) {
     return next();
   }
 
+  if (
+    req.originalUrl.startsWith("/form") ||
+    req.header("Host") == hallyos.config.http.base
+  ) {
+    next();
+    return;
+  }
+
   if (req.query.apiKey != hallyos.config.http.apiKey) {
     hallyos.log.warn(
-      'Not allowed ip (' + getIp(req) + ') tried to access to the site'
+      "Not allowed ip (" + getIp(req) + ") tried to access to the site"
     );
-    res.status(403).json({ error: true, message: 'Forbidden' });
+    res.status(403).json({ error: true, message: "Forbidden" });
     return;
   }
 
