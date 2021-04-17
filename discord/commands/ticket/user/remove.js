b64a90a9670ed71b90ecdf48ec031db6321d7da7
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 20:36:21 +0000

    fixed ticker remove user cmd

--- discord/commands/ticket/user/remove.js
+++ discord/commands/ticket/user/remove.js
@@ -1,17 +1,17 @@
 module.exports = {
-  command: 'remove',
-  description: 'Retirer un utilisateur du ticket',
+  command: "remove",
+  description: "Retirer un utilisateur du ticket",
   usage: "ticket user remove (:tag|<nom d'utilisateur>)",
   roles: hallyos.config.discord.support.roles,
   execute: async (args, channel, cmder, message) => {
-    let ticket = await db('hallyos_tickets').where({
+    let ticket = await db("hallyos_tickets").where({
       channel_id: channel.id,
     });
 
     if (!ticket) {
       const m = await hallyos.discord.client.sendError(
-        'Erreur',
-        'Vous ne pouvez éxecuter cette commande que depuis un ticket !',
+        "Erreur",
+        "Vous ne pouvez éxecuter cette commande que depuis un ticket !",
         channel
       );
       setTimeout(() => {
@@ -29,12 +29,12 @@ module.exports = {
     guild.members.cache.array().forEach(async (member) => {
       if (
         args[0] == member.user.username ||
-        args[0] == member.user.username + '#' + member.user.discriminator
+        args[0] == member.user.username + "#" + member.user.discriminator
       ) {
         find = true;
 
-        const retrieved = await db('hallyos_tickets_externals').where({
-          ticket_id: channel.id,
+        const retrieved = await db("hallyos_tickets_externals").where({
+          ticket_id: ticket.id,
           discord_id: member.id,
         });
 
@@ -51,8 +51,8 @@ module.exports = {
             .then(async () => {
               hallyos.helpers.record.discordAction(
                 cmder,
-                'REMOVE_USER_TO_TICKET',
-                args[0] + ';' + ticket.id
+                "REMOVE_USER_TO_TICKET",
+                args[0] + ";" + ticket.id
               );
               hallyos.discord.client.sendInfo(
                 `Mise à jour participants`,
@@ -63,7 +63,7 @@ module.exports = {
                 `Successfully removed ${member.user.username} from ticket ` +
                   ticket.id
               );
-              await db('hallyos_tickets_externals')
+              await db("hallyos_tickets_externals")
                 .where({
                   ticket_id: ticket.id,
                   discord_id: member.id,
@@ -72,7 +72,7 @@ module.exports = {
             });
         } else {
           const m = await hallyos.discord.client.sendError(
-            'Erreur',
+            "Erreur",
             `L'utilisateur ${member.user.username} n'est pas présent ici !`,
             channel
           );
@@ -89,8 +89,8 @@ module.exports = {
 
     if (!find) {
       const message = await hallyos.discord.client.sendWarn(
-        'Utilisateur introuvable',
-        'Cette utilisateur est introuvable !',
+        "Utilisateur introuvable",
+        "Cette utilisateur est introuvable !",
         channel
       );
       setTimeout(() => {
